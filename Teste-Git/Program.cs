30cbf82a6aba334dfcca7656b2c31b3ef82a46b0
murilo.ruiz91@gmail.com <murilo_o@yahoo.com.br>
2018-10-29 18:59:35 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ Teste-Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo!");
+        }
+    }
+}
