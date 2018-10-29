0ffb25e7f691f049eab2930d0c5f4010a15b45b1
murilo.ruiz91@gmail.com <murilo_o@yahoo.com.br>
2018-10-29 19:02:28 -0300

    Acresentada uma mensagem de bom dia

--- Teste-Git/Program.cs
+++ Teste-Git/Program.cs
@@ -7,6 +7,9 @@ namespace Teste_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Ola mundo!");
+            Console.WriteLine("Bom dia!");
+
+            Console.ReadLine();
         }
     }
 }
